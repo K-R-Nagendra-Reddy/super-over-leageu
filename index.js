2990cdfe5fb7bca6b01e05f620822a3d14afea91
K-R-Nagendra-Reddy <nagendrakothurureddivari@gmail.com>
2023-11-30 19:37:20 +0000

    solution

---
+++ index.js
@@ -0,0 +1,11 @@
+const element = (
+//   Write your code here.
+<div className = "bg-container">
+    <h1 className="heading">Super Over League</h1>
+    <img className="image col-12 col-md-5"   src="https://assets.ccbp.in/frontend/content/react-js/super-over-league-sm-output.png" />
+    <img  className="image col-12 col-md-5" src="https://assets.ccbp.in/frontend/content/react-js/super-over-league-lg-output.png" />
+
+</div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
